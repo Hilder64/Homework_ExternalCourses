efe103f09a6406ca78db9811a53db3b62d9856aa
Hilder64 <aleksei.nimich@gmail.com>
2019-02-14 18:42:41 +0200

    Added initialization interface with text constants

---
+++ src/task_3_regex/view/TextConstants.java
@@ -0,0 +1,26 @@
+package task_3_regex.view;
+
+public interface TextConstants {
+    public static final String SPACE_SING = " ";
+    public static final String DOT_SING = ".";
+    public static final String INPUT_STRING = "Please input your ";
+    public static final String FIRST_NAME = "First name ";
+    public static final String LAST_NAME = "Last name ";
+    public static final String MIDDLE_NAME = "Middle name ";
+    public static final String NICK_NAME = "Nickname ";
+    public static final String COMMENT = "Comment ";
+    public static final String GROUP = "Group ";
+    public static final String HOME_PHONE = "Home phone ";
+    public static final String MOBILE_PHONE = "Mobile phone";
+    public static final String MOBILE_PHONE_ADD = "Second mobile phone ";
+    public static final String E_MAIL = "E-mail ";
+    public static final String SKYPE = "Skype ";
+    public static final String ADDRESS = "Address ";
+    public static final String POSTCODE = "Postcode ";
+    public static final String CITY = "City ";
+    public static final String STREET = "Street ";
+    public static final String HOUSE_NUMBER = "House number ";
+    public static final String APARTMENT_NUMBER = "Apartment number ";
+    public static final String DATE_OF_ENTRY = "Date of entry ";
+    public static final String DATE_OF_LAST_CHANGE = "Date of last change ";
+}
