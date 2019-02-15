48459d36a857fe15b17a224adf60cea2f47d8242
Hilder64 <aleksei.nimich@gmail.com>
2019-02-15 10:03:45 +0200

    Changed some constants

--- src/task_3_regex/view/TextConstants.java
+++ src/task_3_regex/view/TextConstants.java
@@ -3,7 +3,7 @@ package task_3_regex.view;
 public interface TextConstants {
     public static final String SPACE_SING = " ";
     public static final String DOT_SING = ".";
-    public static final String INPUT_STRING = "Please input your ";
+    public static final String INPUT_STRING = "Input your ";
     public static final String FIRST_NAME = "First name ";
     public static final String LAST_NAME = "Last name ";
     public static final String MIDDLE_NAME = "Middle name ";
@@ -15,7 +15,6 @@ public interface TextConstants {
     public static final String MOBILE_PHONE_ADD = "Second mobile phone ";
     public static final String E_MAIL = "E-mail ";
     public static final String SKYPE = "Skype ";
-    public static final String ADDRESS = "Address ";
     public static final String POSTCODE = "Postcode ";
     public static final String CITY = "City ";
     public static final String STREET = "Street ";
@@ -23,4 +22,5 @@ public interface TextConstants {
     public static final String APARTMENT_NUMBER = "Apartment number ";
     public static final String DATE_OF_ENTRY = "Date of entry ";
     public static final String DATE_OF_LAST_CHANGE = "Date of last change ";
+    public static final String WRONG_INPUT_DATA = "Invalid input! Repeat please!\n";
 }
