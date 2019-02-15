23d199f9176c8c27295da6acc7ed0af8eeef9278
Hilder64 <aleksei.nimich@gmail.com>
2019-02-15 10:06:51 +0200

    Added methos that returns Group object

--- src/task_3_regex/model/entity/Group.java
+++ src/task_3_regex/model/entity/Group.java
@@ -5,5 +5,22 @@ public enum Group {
     GROUP2,
     GROUP3,
     GROUP4,
-    GROUP5
+    GROUP5;
+
+    public static Group getGroup(String group) {
+        switch (group.charAt(5)) {
+            case '1':
+                return GROUP1;
+            case '2':
+                return GROUP2;
+            case '3':
+                return GROUP3;
+            case '4':
+                return GROUP4;
+            case '5':
+                return GROUP5;
+            default:
+                throw new RuntimeException();
+        }
+    }
 }
